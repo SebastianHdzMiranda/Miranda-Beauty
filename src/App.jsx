d727c6fc7c46166a8631337062949ddb6f6cb807
Sebastian <sebashm68@gmail.com>
2025-01-05 23:04:26 -0600

    cambiar duration de animacion entre pages

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
                 initial={{ opacity: 0 }}
                 animate={{ opacity: 1, y: 0 }}
                 exit={{ opacity: 0 }}
-                transition={{ duration: 0.5, ease: 'easeInOut' }}
+                transition={{ duration: 0.3, ease: 'easeInOut' }}
               >
                 <Home />
               </motion.div>
@@ -33,7 +33,7 @@ function App() {
                 initial={{ opacity: 0 }}
                 animate={{ opacity: 1, y: 0 }}
                 exit={{ opacity: 0 }}
-                transition={{ duration: 0.5, ease: 'easeInOut' }}
+                transition={{ duration: 0.3, ease: 'easeInOut' }}
               >
                 <Nosotros />
               </motion.div>
