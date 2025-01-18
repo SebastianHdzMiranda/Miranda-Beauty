8d4973b45faf85ad234e6ca3561c624dcde61d13
Sebastian <sebashm68@gmail.com>
2025-01-18 09:55:24 -0600

    Añadir listadoServicios

--- src/components/Listado_Servicios.jsx
+++ src/components/Listado_Servicios.jsx
@@ -7,7 +7,6 @@ function Listado_Servicios({serviciosListado}) {
             <div key={i} className="listado-servicios__servicio">
                 <div className="listado-servicios__header">
                     <h2 className="listado-servicios__heading">{servicio.heading}</h2>
-                    <p className="listado-servicios__precio">{servicio.price}</p>
                 </div>
                 <p className="listado-servicios__texto">{servicio.description}</p>
             </div>
